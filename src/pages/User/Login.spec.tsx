e280902d869b2c442a5ab2d49ede448e56c1513a
Zhang Jing <zhangbuzz@yunkecn.com>
2021-10-18 17:23:54 +0800

    jest fix

--- src/pages/User/Login.spec.tsx
+++ src/pages/User/Login.spec.tsx
@@ -9,26 +9,26 @@ configure({ adapter: new Adapter() });
 
 describe('<Login /> 组件测试', () => {
   it('应该有标题栏', () => {
-    const pageMounted = mount(<Login history={{ push: () => {} }} />);
+    const pageMounted = mount(<Login />);
 
     // 检查标题栏是否有标题
     expect(pageMounted.find('h1').text()).toEqual('React Vscode Template');
   });
   it('发送验证码', async () => {
-    const wrapper = mount(<Login history={{ push: () => {} }} />);
+    const wrapper = mount(<Login />);
     const { onGetCaptcha } = wrapper.find(ProFormCaptcha).props() as ProFormCaptchaProps;
     await act(async () => {
       await onGetCaptcha('13912345678');
     });
   });
   it('登录成功', async () => {
-    const wrapper = shallow(<Login history={{ push: () => {} }} />);
+    const wrapper = shallow(<Login />);
     // const wrapper = mount(<Login history={{ push: () => {} }} />);
     const { onFinish } = wrapper.find(ProForm).props() as ProFormProps;
     await onFinish({ phone: '13912345678', captcha: 'admin' });
   });
   it('登录失败', async () => {
-    const wrapper = mount(<Login history={{ push: () => {} }} />);
+    const wrapper = mount(<Login />);
     const { onFinish } = wrapper.find(ProForm).props() as ProFormProps;
     await onFinish({ phone: '13912345678', captcha: 'wrong' });
   });
