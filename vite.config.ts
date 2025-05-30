f05cf34d7d93a52e0b7cf737ec00c644b7605bcd
fengerzh <buzz.zhang@gmail.com>
2025-05-30 11:30:04 +0800

    更新 package.json，添加 @types/node、@types/react 和 rc-field-form 依赖；修改 vite.config.ts，优化 antd 样式引入和构建配置；更新 yarn.lock 以反映依赖变化。

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,21 @@ export default defineConfig({
       libList: [
         // antd 5.x 不再需要按需引入 less 样式，官方推荐全局引入 reset.css
         // 如有其它库需要按需引入样式，可在此添加
+        {
+          libName: 'antd',
+          style: (name) => {
+              if (name === 'col' || name === 'row') {
+                  return 'antd/lib/style/index.js'
+              }
+              return `antd/es/${name}/style/index.js`
+          },
+        },
       ],
     }),
   ],
   resolve: {
     alias: {
-      '~antd': path.resolve(__dirname, '/node_modules/antd'),
+      // '~antd': path.resolve(__dirname, '/node_modules/antd'),
       '@': path.resolve(__dirname, '/src'),
     },
   },
@@ -22,6 +31,17 @@ export default defineConfig({
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: (id) => {
+        // 排除 Ant Design 组件的 CSS 导入
+        if (id.includes('antd/es') && id.endsWith('.css.js')) {
+          return true;
+        }
+        return false;
       },
     },
   },
